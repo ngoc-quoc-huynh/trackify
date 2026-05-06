3ce47e8b69c09ec93a5d765e525365a477cf9c86
Ngoc Quoc Huynh <ngoc.quoci.huynh@gmail.com>
2026-05-06 23:47:01 +0200

    Add AddExpenseBloc tests

---
+++ test/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:trackify/domain/interfaces/expense.dart';
+
+final class MockExpenseRepository extends Mock implements ExpenseRepository {}
---
+++ test/ui/blocs/add_expense_test.dart
@@ -0,0 +1,55 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:decimal/decimal.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:trackify/domain/models/category.dart';
+import 'package:trackify/domain/models/expense.dart';
+import 'package:trackify/domain/models/new_expense.dart';
+import 'package:trackify/ui/blocs/add_expense/bloc.dart';
+
+import '../../mocks.dart';
+
+void main() {
+  final repository = MockExpenseRepository();
+  AddExpenseBloc createBloc() => AddExpenseBloc(repository);
+  final newExpense = NewExpense(
+    description: '',
+    value: Decimal.one,
+    category: ExpenseCategory.other,
+    date: DateTime.utc(2026),
+  );
+  final expense = Expense(
+    id: 1,
+    description: '',
+    value: Decimal.one,
+    category: ExpenseCategory.other,
+    date: DateTime.utc(2026),
+  );
+
+  test(
+    'inital state is AddExpenseInitial',
+    () => expect(createBloc().state, const AddExpenseInitial()),
+  );
+
+  blocTest<AddExpenseBloc, AddExpenseState>(
+    'emits AddExpenseLoadOnSuccess',
+    setUp: () => when(
+      () => repository.save(newExpense),
+    ).thenAnswer((_) async => expense),
+    build: createBloc,
+    act: (bloc) => bloc.add(
+      AddExpenseEvent(
+        NewExpense(
+          description: '',
+          value: Decimal.one,
+          category: ExpenseCategory.other,
+          date: DateTime.utc(2026),
+        ),
+      ),
+    ),
+    expect: () => [
+      const AddExpenseLoadInProgress(),
+      AddExpenseLoadOnSuccess(expense),
+    ],
+  );
+}
